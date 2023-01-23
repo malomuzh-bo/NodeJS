2ed1447e60098a04f41cf101c55ddcdf1008a10f
malomuzh-bo <malomuzh.bo@gmail.com>
2023-01-23 23:07:27 +0200

    commit

--- Lab2301/homework.js
+++ Lab2301/homework.js
@@ -4,7 +4,7 @@ var port = 7777;
 http.createServer(function(request, response){
     var myURL = new URL(request.url.toString(), `https://${host}:${port}/`);
     response.setHeader('Content-type', 'text/html; charset=utf-8');
-    response.text = "Host: " + myURL.host + "\n" + "Path: " + myURL.pathname + "\n" + "Port: " + myURL.port + "\n" + "Search params: " + myUrl.searchParams;
+    response.text = "Host: " + myURL.host + "\n" + "Path: " + myURL.pathname + "\n" + "Port: " + myURL.port + "\n" + "Search params: " + myURL.searchParams;
     response.end(response.text);
 }).listen(port, host, function()
 {
